2681b6133e59a7a29c5b238ca77ebd51b0e272b7
khorlim <khorlim2001@gmail.com>
2025-07-07 18:37:50 +0800

    update backup func

--- lib/src/shared_pref_helper.dart
+++ lib/src/shared_pref_helper.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'shared_pref_helper_exceptions.dart';
 
 class SharedPrefHelper {
+  static const _backupSuffix = '.bak';
   static final _helperInstance = SharedPrefHelper._internal();
   factory SharedPrefHelper() => _helperInstance;
   SharedPrefHelper._internal();
@@ -17,6 +22,59 @@ class SharedPrefHelper {
 
   Future<void> init() async {
     if (_instance != null) return;
-    _instance = await SharedPreferences.getInstance();
+    try {
+      _instance = await SharedPreferences.getInstance().timeout(
+        const Duration(seconds: 2),
+        onTimeout: () {
+          throw SharedPrefHelperExceptions('SharedPrefHelper not initialized');
+        },
+      );
+      backup();
+    } catch (e) {
+      debugPrint('🔄 SharedPreferences Error: $e');
+      await _attemptRestore();
+
+      try {
+        _instance = await SharedPreferences.getInstance();
+      } catch (e) {
+        debugPrint('🔄 SharedPreferences still failing, deleting file...');
+        final file = await _getWindowsPrefsFile();
+        await file.delete();
+        _instance = await SharedPreferences.getInstance();
+      }
+    }
+  }
+
+  static Future<void> backup() async {
+    debugPrint('🔄 Backing up SharedPreferences...');
+    final file = await _getWindowsPrefsFile();
+    final backup = await _getBackupFile();
+    if (await file.exists()) {
+      await file.copy(backup.path);
+    }
+  }
+
+  static Future<File> _getWindowsPrefsFile() async {
+    final dir = await getApplicationSupportDirectory();
+    return File('${dir.path}/shared_preferences.json');
+  }
+
+  static Future<File> _getBackupFile() async {
+    final prefs = await _getWindowsPrefsFile();
+    return File('${prefs.path}$_backupSuffix');
+  }
+
+  static Future<void> _attemptRestore() async {
+    debugPrint('🔄 Restoring SharedPreferences from backup...');
+    final file = await _getWindowsPrefsFile();
+    final backup = await _getBackupFile();
+
+    if (await backup.exists()) {
+      debugPrint("✅ Restoring SharedPreferences from backup.");
+      await backup.copy(file.path);
+    } else {
+      debugPrint("❌ No backup found. Resetting SharedPreferences.");
+      await file.delete();
+    }
   }
 }
